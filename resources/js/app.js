4079f86cd29f4b3997989e2dda1f00c872885162
Emad Aldin <eabdulwahid@wallpostdev.com>
2023-08-11 22:08:34 +0400

    install vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,16 @@ import './bootstrap';
 import 'admin-lte/dist/js/adminlte.min.js';
 import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
 import 'admin-lte/plugins/jquery/jquery.min.js';
+
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import { createRouter, createWebHistory } from 'vue-router';
+import Routes from './routes.js';
+const app = createApp({});
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: Routes,
+});
+
+app.use(router);
+app.mount('#app');
