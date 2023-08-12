e0f7449ed8eff523a6fefc8d333f1f9d8410e7aa
Emad Aldin <eabdulwahid@wallpostdev.com>
2023-08-12 20:16:15 +0400

    list users

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -1,3 +1,20 @@
+<script setup>
+    import { ref, onMounted } from 'vue';
+
+    const users = ref({});
+    const getUsers = () => {
+        axios.get('/api/users')
+        .then((response) => {
+            users.value = response.data;
+            console.log(users);
+        })
+    }
+
+    onMounted(() => {
+        getUsers();
+    });
+
+</script>
 <template>
     <div>
         <div class="content-header">
@@ -19,7 +36,32 @@
         </div>
         <div class="content">
             <div class="container-fluid">
-                
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Registration Date</th>
+                                    <th>Role</th>
+                                    <th>Options</th>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="(user, index) in users" :key="user.id">
+                                        <td>{{++index}}</td>
+                                        <td>{{ user.name }}</td>
+                                        <td>{{ user.email}}</td>
+                                        <td>{{ user.created_at }}</td>
+                                        <td>role</td>
+                                        <td>options</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApplicationController;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,8 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/admin/dashboard', function() {
-//     return view('dashboard');
-// });
+Route::get('/api/users', [UserController::class, 'index']);
 
 Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
